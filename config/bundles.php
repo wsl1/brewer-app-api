748811d2bfae4c4ecc63bafd4344df8d409f75bd
Wojciech Słomczyński <w.sl@o2.pl>
2018-09-23 18:29:53 +0200

    add cors bundle

--- config/bundles.php
+++ config/bundles.php
@@ -14,5 +14,6 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
-    JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true]
+    JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
