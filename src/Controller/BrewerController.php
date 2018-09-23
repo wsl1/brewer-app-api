3bdf46e7002231f2af1a247d172a0ff1eb563721
Wojciech Słomczyński <w.sl@o2.pl>
2018-09-23 13:15:39 +0200

    implement addBrewers method

--- src/Controller/BrewerController.php
+++ src/Controller/BrewerController.php
@@ -8,8 +8,23 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+
 class BrewerController extends Controller
 {
+    /**
+     * @Route("/brewers")
+     */
+    public function getBrewers() {
+        $serializer = $this->get('jms_serializer');
+
+        $brewerRepository = $this->getDoctrine()->getRepository(Brewer::class);
+        $brewer = $brewerRepository->findAllBrewers();
+
+        $response = $serializer->serialize($brewer, 'json');
+        return new Response($response);
+    }
+
     /**
      * @Route("/brewers/{id}", name="brewers", requirements={"id"="\d+"})
      */
